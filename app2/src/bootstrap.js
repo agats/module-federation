feb81dcb5cd6d4581c531e477dd4a6f02e6a8d67
Andrew Gatlabayan <andrew.gatlabayan@gmail.com>
2020-11-27 16:05:09 -0800

    feat: 🎸 sharing component between two apps
    In this use case, app1 and app2 are using the Counter component from the
    app2 project.

--- app2/src/bootstrap.js
+++ app2/src/bootstrap.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './components/Counter';
+
 
 function App() {
-  return <h1>Hello from second app</h1>;
+  const [count, setCount] = useState(0);
+return (
+    <>
+        <h1>Hello from second app</h1>
+        <Counter
+          count={count}
+          onIncrement={() => setCount(count + 1)}
+          onDecrement={() => setCount(count - 1)}
+        />
+    </>
+    );
 }
 
 export default App;
